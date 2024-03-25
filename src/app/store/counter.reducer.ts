fb3d3dd6992efa852f241e296c1b1f554ea74f53
Inesbourguibaweb <inesbourguibaweb@gmail.com>
2024-03-25 21:24:24 +0100

    add action

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -7,16 +7,19 @@ const initialState = 0;
 /**
  * used to create reducer
  */
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state) => state + 1)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value)
+// );
 
 /**
  * function to create a reducer used in more ancient versions
  * @param state
  * @returns state
  */
-// export function counterReducer(state = initialState){
-//     return state;
-// }
+export function counterReducer(state = initialState, action: any) {
+  if (action.type === '[Counter] Increment') {
+    return state + action.value;
+  }
+  return state;
+}
